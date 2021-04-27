b9abf1c4bacb76f846c76b893b5502f176887efc
bayleyegn100 <bayleyegn100@gmail.com>
2021-04-26 17:47:48 -0700

    create api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,66 @@
+const db = require("../models");
+const router = require("express").Router();
 // Add new exercises to a new workout plan.
-app.post("/", (req, res) => {
-    
+router.put("/workouts/:id", async (req, res) => {
+    try {
+        const updatedWorkout = await db.Workout.findByIdAndUpdate(req.params.id, {
+            $push: {
+                exercises: req.body
+            }
+        }, {
+            new: true
+        });
+        res.json(updatedWorkout);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
 })
-// Add exercises to the most recent workout plan.
-app.post("/", (req, res) => {
-    
-})
-// View the combined weight of multiple exercises from the past seven workouts on the `stats` page.
-app.get("/", (req, res) => {
-    
-})
-// View the total duration of each workout from the past seven workouts on the `stats` page.
-app.get("/", (req, res) => {
-    
+// Adding new workout
+router.post("/workouts", async (req, res) => {
+    try {
+        const newWorkout = await db.Workout.create(req.body);
+        res.json(newWorkout);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
+// getWorkoutsInRange
+router.get("/workouts/range", async (req, res) => {
+try {
+    const workouts = await db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ]).limit(7);
+ res.json(workouts);
+
+}catch (err) {
+    res.status(500).json(err);
+}
+})
+
+// Gettin all workout EX.
+router.get("/workouts", async (req, res) => {
+    try {
+        const workouts = await db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration"
+                    }
+                }
+            }
+        ]);
+        res.json(workouts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
