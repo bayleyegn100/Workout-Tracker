ebc638c5019ee9e4225b9104b3493e5fcddc9030
bayleyegn100 <bayleyegn100@gmail.com>
2021-04-26 19:37:25 -0700

    inreaching the index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 const apiRoutes = require("./apiRoutes")
 const htmlRoutes = require("./htmlRoutes")
-
+// path handler
 router.use("/api", apiRoutes);
 router.use("/", htmlRoutes);
 module.exports = router;
